a3c5eeb4e105be71d53ece0976535769d6a7bc35
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-06-04 03:12:56 -0400

    depoly hello world

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app =  express();
 
 require('./config.js').init(app);
 require('./routes.js').init(app);
+var port =  Number(process.env.PORT || 3000);
 
-app.listen(3000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 console.log("Initalized API");
