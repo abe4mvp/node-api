4fa604f215c1216496dcd63878adb9e90086fbe8
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-06-05 01:31:04 -0400

    added redis and orm

--- server.js
+++ server.js
@@ -3,6 +3,24 @@ var app =  express();
 
 require('./config.js').init(app);
 require('./routes.js').init(app);
+
+
+
+var redis = require('redis');
+var client = redis.createClient();
+client.on('error', function (err) {
+  console.log('Error ' + err);
+});
+
+var caminte = require('caminte');
+var Schema = caminte.Schema;
+var db = {
+ driver     : "redis",
+ host       : "localhost",
+ port       : "6379"
+};
+var schema = new Schema(db.driver, db);
+
 var port =  Number(process.env.PORT || 3000);
 
 app.listen(port, function() {
