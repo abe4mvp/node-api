64f1e35e1ca2db00579dce166cfed95254c0baaa
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-06-06 01:48:52 -0400

    add url

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app =  express();
 
 var redis = require('redis');
 // var client = redis.createClient();
-// var url = require('url');
+var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
