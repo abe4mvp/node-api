be7d919511871e937cb0b06c5d1c5cfd6e00b5ac
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-06-10 17:24:58 -0400

    log redis url

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ var redis = require('redis');
 // 
 if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  console.log("reddddddd", rtg.port, rtg.hostname);
   var client = redis.createClient(rtg.port, rtg.hostname);
   client.auth(rtg.auth.split(":")[1]);
   console.log("production");
